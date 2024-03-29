21b6af2508973e9fdb5a3ec748fc018eade40ed2
caoimhe <caoimh3cahill@gmail.com>
2024-03-29 19:45:39 +0000

    transferring over html

--- src/pages/edit_profile.php
+++ src/pages/edit_profile.php
@@ -44,7 +44,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     // Update the user currently logged in profile table in the database
     $user_id = $_SESSION['id']; //we use this from being logged in
 
-    
+
     // Call setter methods to make the updates in db
     setBio($user_id, $bio);
     setGender($user_id, $gender);
@@ -64,81 +64,194 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Edit Profile</title>
 
-    <!-- @frontend made v basic html page for edit profile. styling and making it look nice should be done here -->
+    <!-- Bootstrap Stylesheet -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+
+    <!-- Bootstrap Icon -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
+
+    <link rel="stylesheet" type="text/css" href="../assets/css/edit_profile.css">
 </head>
+
 <body>
-    <h1>Edit Profile</h1>
 
-        <form action="edit_profile.php" method="post" enctype="multipart/form-data">
+    <!-- Start of Navbar -->
+    <nav class="navbar navbar-fixed-top" id="navbar">
 
-       <!-- Name -->
-        <label for="name">Name:</label>
-        <span id="name"><?php echo htmlspecialchars($name); ?></span>
-        <!-- Age -->
-        <label for="age">Age:</label>
-        <input type="number" id="age" name="age" <?php if(isset($age)) echo "value='$age'"; ?> <?php if(isset($age)) echo "readonly"; ?> required>
+        <!-- Images -->
+        <div class="images">
+            <img class="header-img" src="../assets/images/ul_logo.png" alt="ul_logo">
+            <div class="line"></div>
+            <img class="header-img" src="../assets/images/ulSinglesTrasparent.png" alt="ulSingles_logo">
+        </div>
 
-        <!-- Gender -->
-        <label for="gender">Gender:</label>
-        <select id="gender" name="gender" <?php if(isset($gender)) echo "disabled"; ?> required>
-            <option value="Male" <?php if(isset($gender) && $gender == "Male") echo "selected"; ?>>Male</option>
-            <option value="Female" <?php if(isset($gender) && $gender == "Female") echo "selected"; ?>>Female</option>
-            <option value="Other" <?php if(isset($gender) && $gender == "Other") echo "selected"; ?>>Other</option>
-        </select>
+        <!-- Buttons -->
+        <div class="btn-group ms-auto" role="group">
+            <button type="button" id="explorebutton" class="btn button d-none d-md-block" onclick="location.href='explore.php'">Explore</button>
+            <button type="button" id="logoutbutton" class="btn button d-none d-md-block" onclick="location.href='logout.php'">Log Out</button>
+        </div>
 
-        <!-- Hobbies (May need to update this as text for it is meh)-->
-        <label for="hobbies">Hobbies:</label>
-        <input type="text" id="hobbies" name="hobbies" required value="<?php echo htmlspecialchars($hobbies); ?>">
+        <!-- Profile Icon -->
+        <div class="dropdown">
+            <button class="btn-secondary" id="iconbutton" type="button" data-bs-toggle="dropdown" aria-expanded="false">
+                <svg xmlns="http://www.w3.org/2000/svg" width="45" height="40" fill="currentColor" class="bi bi-person-circle" viewBox="0 0 16 16">
+                    <path d="M11 6a3 3 0 1 1-6 0 3 3 0 0 1 6 0" />
+                    <path fill-rule="evenodd" d="M0 8a8 8 0 1 1 16 0A8 8 0 0 1 0 8m8-7a7 7 0 0 0-5.468 11.37C3.242 11.226 4.805 10 8 10s4.757 1.225 5.468 2.37A7 7 0 0 0 8 1" />
+                </svg>
+            </button>
+            <ul class="dropdown-menu" aria-labelledby="iconbutton" id="profiledropdown">
+                <li><a class="dropdown-item-profile" href="edit_profile.php">Edit Profile</a></li>
+                <li><a class="dropdown-item-profile d-md-none" href="logout.php">Log Out</a></li>
+            </ul>
+        </div>
 
-        <!-- Bio -->
-        <label for="bio">Bio:</label>
-        <textarea id="bio" name="bio" required><?php echo htmlspecialchars($bio); ?></textarea>
+    </nav>
+    <!-- End of Navbar -->
 
-        <!-- Profile Picture-->
-        <label for="profile_pic">Profile Picture:</label>
-        <input type="file" id="profile_pic" name="profile_pic">
-        <img src="<?php echo $profile_pic_filename ? '/' . htmlspecialchars($profile_pic_filename) : '/path/to/default/image.png'; ?>" alt="Profile Picture" style="max-width:200px;">
+    <!-- Dropdown Menu Button -->
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col-12">
+                <button class="btn btn-primary dropdown-toggle" style="background-color: #e6d1be;" type="button" id="menu-dropdown" data-bs-toggle="dropdown">
+                    Edit Profile
+                </button>
 
-        
-        <!--  pursuing -->
-        <label for="pursuing">Pursuing:</label>
-        <select id="pursuing" name="pursuing" required>
-            <option value="Male" <?php if ($pursuing == "Male") echo "selected"; ?>>Male</option>
-            <option value="Female" <?php if ($pursuing == "Female") echo "selected"; ?>>Female</option>
-            <option value="Other"<?php if ($pursuing == "Other") echo "selected"; ?>>Other</option>
-        </select>
+                <ul class="dropdown-menu" aria-labelledby="menu-dropdown" id="homedropdown">
+                    <li><a class="dropdown-item" href="home.php">Home</a></li>
+                    <li><a class="dropdown-item" href="explore.php">Explore</a></li>
+                    <li><a class="dropdown-item" href="matches.php">Matches</a></li>
+                    <li><a class="dropdown-item" href="messages.php">Messages</a></li>
+                </ul>
+            </div>
+        </div>
+    </div>
 
-        <!-- College Year -->
-        <label for="college_year">College Year:</label>
-        <select id="college_year" name="college_year" required>
-            <option value="Undergrad" <?php if ($college_year == "Undergrad") echo "selected"; ?>>Undergrad</option>
-            <option value="Masters" <?php if ($college_year == "Masters") echo "selected"; ?>>Masters</option>
-            <option value="PhD" <?php if ($college_year == "PhD") echo "selected"; ?>>PhD</option>
-        </select>
+    <br>
 
-        <!-- COurse-->
-        <label for="course">Course:</label>
-        <input type="text" id="course" name="course" required value="<?php echo htmlspecialchars($course); ?>">
+    <div class="container-fluid border border-2" id="outline">
+        <div class="row">
+            <form class="container-fluid" action="edit_profile.php" method="post" enctype="multipart/form-data">
+                <div class="row">
+                    <div class="col-7 order-2">
 
-        <!-- Looking for-->
-        <label for="looking_for">Looking For:</label>
-        <select id="looking_for" name="looking_for" required>
-            <option value="Short-term" <?php if ($looking_for == "Short-term") echo "selected"; ?>>Short-Term</option>
-            <option value="Long-term"<?php if ($looking_for == "Long-term") echo "selected"; ?>>Long-Term</option>
-            <option value="Unsure" <?php if ($looking_for == "Unsure") echo "selected"; ?>>Unsure</option>
-        </select>
+                        <div class="row">
+                            <div class="col-4">
+                                <!-- Name -->
+                                <label for="name">Name:</label>
+                                <span id="name"><?php echo htmlspecialchars($name); ?></span>
+                            </div>
+                            <div class="col-4">
+                                <!-- Age -->
+                                <label for="age">Age:</label>
+                                <input type="number" id="age" name="age" <?php if (isset($age)) echo "value='$age'"; ?> <?php if (isset($age)) echo "readonly"; ?> required>
+                            </div>
 
-        <!-- Button to just update changes made in db -->
-        <button type="submit">Save Changes</button>
-    </form>
+                            <div class="col-4">
+                                <!-- Gender -->
+                                <label for="gender">Gender:</label>
+                                <select id="gender" name="gender" <?php if (isset($gender)) echo "disabled"; ?> required>
+                                    <option value="Male" <?php if (isset($gender) && $gender == "Male") echo "selected"; ?>>Male</option>
+                                    <option value="Female" <?php if (isset($gender) && $gender == "Female") echo "selected"; ?>>Female</option>
+                                    <option value="Other" <?php if (isset($gender) && $gender == "Other") echo "selected"; ?>>Other</option>
+                                </select>
+                            </div>
 
-    
+                        </div>
+
+                        <div class="row">
+                            <div class="col-6">
+                                <!-- College Year -->
+                                <label for="college_year">College Year:</label>
+                                <select id="college_year" name="college_year" required>
+                                    <option value="Undergrad" <?php if ($college_year == "Undergrad") echo "selected"; ?>>Undergrad</option>
+                                    <option value="Masters" <?php if ($college_year == "Masters") echo "selected"; ?>>Masters</option>
+                                    <option value="PhD" <?php if ($college_year == "PhD") echo "selected"; ?>>PhD</option>
+                                </select>
+                            </div>
+
+                            <div class="col-6">
+                                <!-- Course-->
+                                <label for="course">Course:</label>
+                                <input type="text" id="course" name="course" required value="<?php echo htmlspecialchars($course); ?>">
+
+                            </div>
+                        </div>
+
+                        <div class="row">
+                            <div class="col-6">
+
+                                <!--  pursuing -->
+                                <label for="pursuing">Pursuing:</label>
+                                <select id="pursuing" name="pursuing" required>
+                                    <option value="Male" <?php if ($pursuing == "Male") echo "selected"; ?>>Male</option>
+                                    <option value="Female" <?php if ($pursuing == "Female") echo "selected"; ?>>Female</option>
+                                    <option value="Other" <?php if ($pursuing == "Other") echo "selected"; ?>>Other</option>
+                                </select>
+                            </div>
+
+                            <div class="col-6">
+                                <!-- Looking for-->
+                                <label for="looking_for">Looking For:</label>
+                                <select id="looking_for" name="looking_for" required>
+                                    <option value="Short-term" <?php if ($looking_for == "Short-term") echo "selected"; ?>>Short-Term</option>
+                                    <option value="Long-term" <?php if ($looking_for == "Long-term") echo "selected"; ?>>Long-Term</option>
+                                    <option value="Unsure" <?php if ($looking_for == "Unsure") echo "selected"; ?>>Unsure</option>
+                                </select>
+                            </div>
+                        </div>
+
+                        <div class="row">
+                            <div class="col-12">
+                                <!-- Bio -->
+                                <label for="bio">Bio:</label>
+                                <textarea id="bio" name="bio" required><?php echo htmlspecialchars($bio); ?></textarea>
+                            </div>
+                        </div>
+
+                        <div class="row">
+                            <div class="col-12">
+                                <!-- Hobbies (May need to update this as text for it is meh)-->
+                                <label for="hobbies">Hobbies:</label>
+                                <input type="text" id="hobbies" name="hobbies" required value="<?php echo htmlspecialchars($hobbies); ?>">
+                            </div>
+                        </div>
+                    </div>
+
+
+                    <div class="col-4 order-1 imgBox">
+                        <!-- Profile Picture-->
+                        <label for="profile_pic" class="fileUploadBtn">Upload/Change profile picture</label>
+                        <input type="file" id="profile_pic" name="profile_pic">
+                        <img src="<?php echo $profile_pic_filename ? '/' . htmlspecialchars($profile_pic_filename) : '/path/to/default/image.png'; ?>" alt="Profile Picture" style="max-width:200px;">
+
+
+                        <!-- Button to just update changes made in db -->
+                        <button type="submit" class="btn btn-secondary mt-2 mb-4 saveChangesBtn">Save Changes</button>
+                    </div>
+                </div>
+            </form>
+
+        </div>
+    </div>
+
+
+
+    <br>
+
+
+
+    <footer class="p-2">
+        © 2024 Copyright UL Singles. All Rights Reserved
+    </footer>
 
 </body>
+
 </html>
