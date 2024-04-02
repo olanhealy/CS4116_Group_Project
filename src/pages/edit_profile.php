e067f16b790111bc1bbfb6a8adf6455ff304b2f2
caoimhe <caoimh3cahill@gmail.com>
2024-04-02 23:17:03 +0100

    started responsiveness and sample code for enums

--- src/pages/edit_profile.php
+++ src/pages/edit_profile.php
@@ -59,8 +59,18 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     header("Location: home.php");
 }
 
-
 ?>
+<!-- 
+<?php
+enum CourseOfStudy : string
+{
+    case COMPUTER_SCIENCE = 'Computer Science';
+    case ENGINEERING = 'Engineering';
+    case BUSINESS = 'Business';
+    case ARTS = 'Arts';
+}
+
+?> -->
 
 <!DOCTYPE html>
 <html lang="en">
@@ -79,6 +89,13 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
 
     <link rel="stylesheet" type="text/css" href="../assets/css/edit_profile.css">
+
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+
+    <!-- Scripts -->
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/select2/4.1.0-beta.0/css/select2.min.css" rel="stylesheet">
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/select2/4.1.0-beta.0/js/select2.min.js"></script>
+
 </head>
 
 <body>
@@ -136,39 +153,39 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 
     <br>
 
-    <div class="container-fluid border border-2" id="outline">
+    <div class="container-fluid border border-2 col-md-12" id="outline">
         <div class="row">
             <form class="container-fluid" action="edit_profile.php" method="post" enctype="multipart/form-data">
                 <div class="row">
-                    <div class="col-7 order-2 info-box">
+                    <div class="col-lg-7 order-lg-2 col-md-12 info-box">
 
                         <div class="row inputField">
-                            <div class="col-4">
+                            <div class="col-md-4 col-sm-4">
                                 <!-- Name -->
                                 <label for="name" class="inputLabelText">Name</label>
                             </div>
-                            <div class="col-4">
+                            <div class="col-md-4 col-sm-4">
                                 <!-- Age -->
                                 <label for="age" class="inputLabelText">Age</label>
                             </div>
 
-                            <div class="col-4">
+                            <div class="col-md-4 col-sm-4">
                                 <!-- Gender -->
                                 <label for="gender" class="inputLabelText">Gender</label>
                             </div>
                         </div>
 
                         <div class="row inputField">
-                            <div class="col-4">
+                            <div class="col-md-4 col-sm-4">
                                 <!-- Name -->
                                 <span id="name"><?php echo htmlspecialchars($name); ?></span>
                             </div>
-                            <div class="col-4">
+                            <div class="col-md-4 col-sm-4">
                                 <!-- Age -->
                                 <input type="number" id="age" name="age" class="textInput" placeholder="Type here..." <?php if (isset($age)) echo "value='$age'"; ?> <?php if (isset($age)) echo "readonly"; ?> required>
                             </div>
 
-                            <div class="col-4">
+                            <div class="col-md-4 col-sm-4">
                                 <!-- Gender -->
                                 <select id="gender" name="gender" class="optionDropdown" <?php if (isset($gender)) echo "disabled"; ?> required>
                                     <option value="" disabled selected>Choose..</option>
@@ -180,19 +197,19 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
                         </div>
 
                         <div class="row inputField">
-                            <div class="col-6">
+                            <div class="col-md-6 col-sm-6">
                                 <!-- College Year -->
                                 <label for="college_year" class="inputLabelText">College Year</label>
                             </div>
 
-                            <div class="col-6">
+                            <div class="col-md-6 col-sm-6">
                                 <!-- Course-->
                                 <label for="course" class="inputLabelText">Course of Study</label>
                             </div>
                         </div>
 
                         <div class="row inputField">
-                            <div class="col-6">
+                            <div class="col-md-6 col-sm-6">
                                 <!-- College Year -->
                                 <select id="college_year" name="college_year" class="optionDropdown" required>
                                     <option value="" disabled selected>Choose..</option>
@@ -202,26 +219,35 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
                                 </select>
                             </div>
 
-                            <div class="col-6">
+                            <div class="col-md-6 col-sm-6">
                                 <!-- Course-->
-                                <input type="text" id="course" name="course" class="textInput" placeholder="Type here..." required value="<?php echo htmlspecialchars($course); ?>">
+                                <!-- <select class="form-control" id="course_of_study" name="course_of_study">
+                                    <?php
+                                    $courses = array_column(CourseOfStudy::cases(), 'value');
+                                    foreach ($courses as $value) {
+                                        echo "<option value=\"$value\">$value</option>";
+                                    }
+                                    ?>
+                                </select> -->
+                                <input type="text" id="course"
+                                 name="course" class="textInput" placeholder="Type here..." required value="<?php echo htmlspecialchars($course); ?>">
                             </div>
                         </div>
 
                         <div class="row inputField">
-                            <div class="col-6">
+                            <div class="col-md-6 col-sm-6">
                                 <!--  pursuing -->
                                 <label for="pursuing" class="inputLabelText">Pursuing</label>
                             </div>
 
-                            <div class="col-6">
+                            <div class="col-md-6 col-sm-6">
                                 <!-- Looking for-->
                                 <label for="looking_for" class="inputLabelText">Looking For</label>
                             </div>
                         </div>
 
                         <div class="row inputField">
-                            <div class="col-6">
+                            <div class="col-md-6 col-sm-6">
                                 <!--  pursuing -->
                                 <select id="pursuing" name="pursuing" class="optionDropdown" required>
                                     <option value="" disabled selected>Choose..</option>
@@ -231,7 +257,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
                                 </select>
                             </div>
 
-                            <div class="col-6">
+                            <div class="col-md-6 col-sm-4">
                                 <!-- Looking for-->
                                 <select id="looking_for" name="looking_for" class="optionDropdown" required>
                                     <option value="" disabled selected>Choose..</option>
@@ -243,21 +269,21 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
                         </div>
 
                         <div class="row inputField">
-                            <div class="col-12">
+                            <div class="col-md-12 col-sm-12">
                                 <!-- Bio -->
                                 <label for="bio" class="inputLabelText">Bio</label>
                             </div>
                         </div>
 
                         <div class="row inputField">
-                            <div class="col-12">
+                            <div class="col-md-12 col-sm-12">
                                 <!-- Bio -->
                                 <textarea id="bio" name="bio" class="textInput" placeholder="Type here..." required><?php echo htmlspecialchars($bio); ?></textarea>
                             </div>
                         </div>
 
                         <div class="row inputField">
-                            <div class="col-12">
+                            <div class="col-md-12 col-sm-4">
                                 <!-- Hobbies (May need to update this as text for it is meh)-->
                                 <label for="hobbies" class="inputLabelText">Hobbies</label>
                             </div>
@@ -265,7 +291,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 
 
                         <div class="row inputField">
-                            <div class="col-12">
+                            <div class="col-md-12 col-sm-4">
                                 <!-- Hobbies (May need to update this as text for it is meh)-->
                                 <input type="text" id="hobbies" name="hobbies" class="textInput" placeholder="Type here..." required value="<?php echo htmlspecialchars($hobbies); ?>">
                             </div>
@@ -273,15 +299,15 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
                     </div>
 
 
-                    <div class="col-5 order-1 imgContainer">
-                            <!-- Profile Picture-->
-                            <img class="profilePicture" src="<?php echo $profile_pic_filename ? '/' . htmlspecialchars($profile_pic_filename) : '/src/assets/images/defaultProfilePic.jpg'; ?>" alt="Profile Picture">
+                    <div class="col-lg-5 order-lg-1 col-md-12 imgContainer">
+                        <!-- Profile Picture-->
+                        <img class="profilePicture" src="<?php echo $profile_pic_filename ? '/' . htmlspecialchars($profile_pic_filename) : '/src/assets/images/defaultProfilePic.jpg'; ?>" alt="Profile Picture">
 
-                            <label for="profile_pic" class="fileUploadBtn">Upload/Change profile picture</label>
-                            <input type="file" id="profile_pic" name="profile_pic">
+                        <label for="profile_pic" class="fileUploadBtn">Upload/Change profile picture</label>
+                        <input type="file" id="profile_pic" name="profile_pic">
 
-                            <!-- Button to just update changes made in db -->
-                            <button type="submit" class="btn btn-secondary mt-2 mb-4 saveChangesBtn">Save Changes</button>
+                        <!-- Button to just update changes made in db -->
+                        <button type="submit" class="btn btn-secondary mt-2 mb-4 saveChangesBtn">Save Changes</button>
                     </div>
                 </div>
             </form>
@@ -298,6 +324,16 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     <footer class="p-2">
         © 2024 Copyright UL Singles. All Rights Reserved
     </footer>
+
+
+    <!-- <script>
+        $(document).ready(function() {
+            $('#course_of_study').select2({
+                placeholder: "Select course",
+                allowClear: true
+            });
+        });
+    </script> -->
 
 </body>
 
