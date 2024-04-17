0f31f7d921c8c596d7075b0535ec77a2966cf0bc
Adam Collins <21332967@studentmail.ul.ie>
2024-04-17 01:26:20 +0100

    Changed ban user to appear as modal instead of seperate page

--- src/pages/admin/showUserAdmin.php
+++ src/pages/admin/showUserAdmin.php
@@ -36,9 +36,9 @@
         if(getUserRole($targetId) == "standard"){
             include "editProfileAdmin.php";
             //add buttons to div
-            echo '<div class="linkContainer">';
+            echo '<div class="col-md-12 col-lg-12 col-lg-12" id="linkContainer">';
             include "makeAdmin.html";
-            echo '<a href="banUser.html" id="banUser" class="btn btn-danger">Ban User</a>';
+            echo '<button type="button" id="banUser" class="btn btn-danger" data-bs-toggle="modal" data-bs-target="#banUserModal">Ban User</button>';
         }else if(getUserRole($targetId) == 'admin'){
             //include page for viewing an admin account
             include "viewAdminAccount.html";
@@ -50,8 +50,76 @@
         //always include footer on all pages
         include "../footer.php";
     }
-    ?>
+?>
 
     <html>
-        <link rel="stylesheet" type="text/css" href="../../assets/css/editProfileAdmin.css">
+        <head>
+            <!-- Style Sheet -->
+            <link rel="stylesheet" type="text/css" href="../../assets/css/showUserAdmin.css">
+        </head>
+
+        <body>
+
+            <!-- Ban User Modal -->
+            <div class="modal fade" id="banUserModal" tabindex="-1" aria-hidden="true">
+                <div class="modal-dialog modal-dialog-centered">
+                    <div class="modal-content">
+
+                        <div class="modal-header">
+                            <h4 style="font-weight: bold;" class="modal-title">Ban User</h4>
+                            <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                        </div>
+
+                        <form action="banUser.php" method="post">
+                            <div class="modal-body">
+                                
+                                <!-- Text Box for Ban Reason -->
+                                <label for="reason">Reason:</label>
+                                <textarea id="banUserTextArea" name="reason" rows="4" cols="50" required></textarea><br><br>
+                                
+                                <!-- Buttons for Ban Duration -->
+                                <div class="row">
+                                    <label for="reason">Date of Unban:</label>
+
+                                    <div class="col-6" id="temporarydiv">
+                                        <input type="date" id="dateOfUnban" name="dateOfUnban"><br><br>
+                                    </div>
+
+                                    <div class="col-6" id="permanentdiv">
+                                        <!-- have this button to set unban date as year 9999 for perm-ban -->
+                                        <input type="hidden" name="permaBan" value="1">
+                                        <input type="submit" id="permaBan" value="PermaBan">
+                                    </div>
+                                    
+                                </div>
+                            </div>
+
+                            <div class="modal-footer">
+                                <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                                <input type="submit" class="btn btn-danger" value="Ban User">
+                            </div>
+                        </form>
+
+                    </div>
+                </div>
+            </div>
+
+            <!-- Permanently Bans User -->
+            <script>
+                document.getElementById('permaBan').addEventListener('click', function() {
+                    document.getElementById('dateOfUnban').required = false;
+                });
+            </script>
+
+            <!-- Doesn't allow the unban date to be before today's date -->
+            <script>
+                // Get today's date
+                var today = new Date().toISOString().split('T')[0];
+
+                // Set the minimum date for the input field
+                document.getElementById("dateOfUnban").setAttribute("min", today);
+                document.getElementById("dateOfUnbanM").setAttribute("min", today);
+             </script>
+
+        </body>
     </html>
