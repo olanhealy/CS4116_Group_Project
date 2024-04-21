8ca312e2cb33ed1757b9efb1249f807418a4c61b
Olan Healy <olanhealy12@gmail.com>
2024-04-21 15:05:02 +0100

    fix: sql_cmds update

--- src/pages/db_connection.php
+++ src/pages/db_connection.php
@@ -1,17 +1,17 @@
 <?php
 
 //used for connecting to the database on server
-$sname= "sql313.infinityfree.com";
-$unmae= "if0_36038205";
-$password = "cs41162024";
-$db_name = "if0_36038205_ulsingles";
+//$sname= "sql313.infinityfree.com";
+//$unmae= "if0_36038205";
+//$password = "cs41162024";
+//$db_name = "if0_36038205_ulsingles";
 
 
 //used for connecting to the database on local machine
-// $sname= "localhost";
-// $unmae= "root";
-// $password = "";
-// $db_name = "ulsingles";
+ $sname= "localhost";
+ $unmae= "root";
+ $password = "";
+ $db_name = "ulsingles";
 
 //connect to the database
 $conn = mysqli_connect($sname, $unmae, $password, $db_name);
