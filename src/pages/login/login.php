6badca3983070d3391ef1310a51ff1a6eaacb523
Olan Healy <olanhealy12@gmail.com>
2024-04-28 00:19:28 +0100

    test: see if favicon icon works

--- src/pages/login/login.php
+++ src/pages/login/login.php
@@ -104,6 +104,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="icon" href="/uISinglesSymbolTransparent.ico" type="image/x-icon">
 
     <!-- Bootstrap Stylesheet -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
