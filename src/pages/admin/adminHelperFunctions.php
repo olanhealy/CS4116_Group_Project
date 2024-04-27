661501307696688709b311869879c096b3119a19
Olan Healy <olanhealy12@gmail.com>
2024-04-27 23:41:20 +0100

    issue #167 fixed for admin too

--- src/pages/admin/adminHelperFunctions.php
+++ src/pages/admin/adminHelperFunctions.php
@@ -6,7 +6,7 @@ function adminAccessCheck()
         session_start();
     }
     if (!isset($_SESSION['user_id']) || getUserRole($_SESSION['user_id']) != "admin") {
-        header("Location: /index.php");
+        header("Location: ../../../index.php");
         exit();
     }
 }
