ce19965d24749b6ee5729fd8c1188bd355419436
caoimhe <caoimh3cahill@gmail.com>
2024-05-01 15:17:15 +0100

    adding icon to header file

--- src/pages/helpers/header.php
+++ src/pages/helpers/header.php
@@ -23,6 +23,7 @@ $totalNotifications = $notifications['messages'] + $notifications['matches'];
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title><?php echo $pageName; ?></title>
+    <link rel="icon" href="/ulSinglesSymbolTransparent.ico" type="image/x-icon">
 
     <!-- Bootstrap Stylesheet -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
