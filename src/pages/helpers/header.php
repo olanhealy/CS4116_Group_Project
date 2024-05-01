8b70c73a2e5bb4caa3fd981b0f5568db11fc3e29
caoimhe <caoimh3cahill@gmail.com>
2024-05-01 15:06:12 +0100

    Adding svg heart from search to other pages

--- src/pages/helpers/header.php
+++ src/pages/helpers/header.php
@@ -116,6 +116,11 @@ $totalNotifications = $notifications['messages'] + $notifications['matches'];
                     <li><a class="dropdown-item" href="/src/pages/messages/messages.php">Messages</a></li>
                     <li><a class="dropdown-item" href="/src/pages/search/searchPage.html">Search</a></li>
                 </ul>
+                <svg class="heartFill" xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill="currentColor"
+                    class="bi bi-heart-fill" viewBox="0 0 16 16">
+                    <path fill-rule="evenodd"
+                        d="M8 1.314C12.438-3.248 23.534 4.735 8 15-7.534 4.736 3.562-3.248 8 1.314" />
+                </svg>
             </div>
         </div>
     </div>
