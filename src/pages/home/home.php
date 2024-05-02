f4e09e810049421411d6007bf1c2469ac3f60fd9
Adam Collins <21332967@studentmail.ul.ie>
2024-05-02 04:53:22 +0100

    Home page frontend clean up

--- src/pages/home/home.php
+++ src/pages/home/home.php
@@ -1,3 +1,5 @@
+<!-- Home Page -->
+
 <?php
 include_once("../helpers/helperFunctions.php");
 include_once("../admin/adminHelperFunctions.php");
@@ -24,25 +26,27 @@ $totalNotifications = $notifications['messages'] + $notifications['matches'];
 
 </head>
 <body>
-<?php 
-    setupHeader();
-    ?>
 
-   
+    <!-- Header & Dropdown Menu -->
+    <?php setupHeader(); ?>
+
     <br>
 
     <!-- Main Container -->
     <div class="container-fluid mx-auto border border-5 col-md-12 col-lg-12 col-sm-12" id="outline">
         <div class="row">
             <div class="col-lg-6" id="leftContainer">
+                <!-- Title -->
                 <div class="container col-lg-12 col-md-12 col-sm-12" style="text-align: center;">
                     <a id="title"> 💚 UL Singles 💚 </a>
                 </div>
-    
+
+                <!-- Current Event -->
                 <div class="container col-lg-12 col-md-12 col-sm-12" style="text-align: center;">
-                    <img id="deal" class="img-fluid" src="../../assets/images/event.jpeg" alt="Current Event">
+                    <img id="event" class="img-fluid" src="../../assets/images/event.jpeg" alt="Current Event">
                 </div>
 
+                <!-- Information -->
                 <div class="container col-lg-9 col-md-7 col-sm-7" style="text-align: center; margin-bottom: 10px;">
                     <a id="info">This week's event is the perfect chance to get to know some of your new matches better! </a>
                     <a>😉</a>
@@ -50,15 +54,13 @@ $totalNotifications = $notifications['messages'] + $notifications['matches'];
 
             </div>
             
+            <!-- Main Image -->
             <div class="col-lg-6 d-lg-flex justify-content-center align-items-center order-lg-2" style="text-align: center">
                 <img id="picture" class="img-fluid" src="../../assets/images/holdinghands.jpg" alt="homepage_image">
             </div>
         </div>
     </div>
     
-    
-
-
     <br>
 
     <!-- Slogan -->
@@ -73,9 +75,7 @@ $totalNotifications = $notifications['messages'] + $notifications['matches'];
     <br>
 
     <!-- Footer -->
-    <footer class="p-2">
-        © 2024 Copyright UL Singles. All Rights Reserved
-    </footer>
+    <?php setupFooter(); ?>
 
 </body>
 </html>
